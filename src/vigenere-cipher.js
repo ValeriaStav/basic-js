43730788ffd1671ffd02c1ef31ed76f2e62cdbe7
ValeriaStav <ronnyramb@gmail.com>
2025-01-06 22:39:26 +0300

    feat: add  Vigenere cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,32 +1,75 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(text, key) {
+    if (!text || !key) throw new Error("Incorrect arguments!");
+
+    let result = "";
+    let j = 0;
+
+    for (let i = 0; i < text.length; i++) {
+      const char = text[i];
+      if (/[A-Za-z]/.test(char)) {
+        const shift = key[j % key.length].toUpperCase().charCodeAt(0) - 65;
+        const base = char === char.toUpperCase() ? 65 : 97;
+        result += String.fromCharCode(
+          ((char.toUpperCase().charCodeAt(0) - 65 + shift) % 26) + base
+        );
+        j++;
+      } else {
+        result += char;
+      }
+    }
+
+    return this.isDirect
+      ? result.toUpperCase()
+      : result.split("").reverse().join("");
+  }
+
+  decrypt(text, key) {
+    if (!text || !key) throw new Error("Incorrect arguments!");
+
+    let result = "";
+    let j = 0;
+
+    for (let i = 0; i < text.length; i++) {
+      const char = text[i];
+      if (/[A-Za-z]/.test(char)) {
+        const shift = key[j % key.length].toUpperCase().charCodeAt(0) - 65;
+        const base = char === char.toUpperCase() ? 65 : 97;
+        result += String.fromCharCode(
+          ((char.toUpperCase().charCodeAt(0) - 65 - shift + 26) % 26) + base
+        );
+        j++;
+      } else {
+        result += char;
+      }
+    }
+
+    return this.isDirect ? result : result.split("").reverse().join("");
   }
 }
 
