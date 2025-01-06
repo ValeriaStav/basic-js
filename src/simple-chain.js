21a299a1bb676f8154cefda9d18929f58f42deed
ValeriaStav <ronnyramb@gmail.com>
2025-01-06 21:49:01 +0300

    feat: add Make chain solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,29 +1,49 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
+
 const chainMaker = {
+  chain: [], // Храним цепочку в виде массива
+
+  // Получаем длину цепочки
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  // Добавляем ссылку в цепочку
+  addLink(value) {
+    this.chain.push(`( ${value} )`);
+    return this; // Возвращаем сам объект, чтобы можно было цепочечно вызывать методы
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  // Удаляем ссылку на указанной позиции (позиции начинаются с 1)
+  removeLink(position) {
+    if (
+      typeof position !== "number" ||
+      position <= 0 ||
+      position > this.chain.length
+    ) {
+      this.chain = []; // Если позиция некорректная, очищаем цепочку
+      throw new Error("You can't remove incorrect link!");
+    }
+    this.chain.splice(position - 1, 1);
+    return this;
   },
+
+  // Переворачиваем цепочку
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
+
+  // Завершаем цепочку, возвращаем её как строку
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const result = this.chain.join("~~");
+    this.chain = []; // Очищаем цепочку для последующих вызовов
+    return result;
   }
 };
 
